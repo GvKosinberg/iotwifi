b14698855032a4f223d27b1185fccba2c18cb2c2
GvKosinberg <666kosyak666@gmail.com>
2018-08-02 09:22:06 +0300

    try with cyclic ip check (5 times with 1 sec delay)

--- iotwifi/wpacfg.go
+++ iotwifi/wpacfg.go
@@ -201,7 +201,15 @@ func (wpa *WpaCfg) ConnectNetwork(creds WpaCredentials) (WpaConnection, error) {
 			// see https://developer.android.com/reference/android/net/wifi/SupplicantState.html
 			if state == "COMPLETED" {
 				// save the config
-				ip := string((rIp.FindSubmatch(stateOut))[1])
+				ip := ""
+				for i := 0; i < 5; i++ {
+					mss := rIp.FindSubmatch(stateOut)
+					if len(mss) > 0 {
+						ip = string([1])
+						continue
+					}
+					time.Sleep(1 * time.Second)
+				}
 				saveOut, err := exec.Command("wpa_cli", "-i", "wlan0", "save_config").Output()
 				if err != nil {
 					wpa.Log.Fatal(err.Error())
